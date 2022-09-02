0338541a2814ba49131adf18c0b1d8d776257968
Maria <marinasol21_5@hotmail.cl>
2022-09-02 10:11:02 -0500

    operaciones basicas

--- intro_to_python.py
+++ intro_to_python.py
@@ -4,5 +4,18 @@ print ("Hola")
 
 if 5 > 2:
     print("Cinco es mayor que dos")
+# La inventacion es importante. Es parte de la sintaxis
+# En caso de la linea 8 debe ir indestada para ser considerada
+# parte de "if"
+# Cuando lleva comillas es texto y no se ejecuta la expresion
+#Cuando no lleva comillas si se ejecuta la expresion
+print(40+2)
+print("40+2")
+
+# Operaciones matematicas
+print(7*6)
+print(40/6)
 
 
+# Si no pongo las comillas en una palabra, es sintaxis invalida
+# print (hola mundo)
---
+++ string_operations.py
@@ -0,0 +1,11 @@
+# String concatenation  (juntar string)
+
+word_one = "hello"
+
+space = " "
+
+word_two = "world"
+
+sentence = word_one + space + word_two
+
+print(sentence) 
